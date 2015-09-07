e785589ef77950351afb1151ff83de8ed6d58fc0
jbrustle <brut1901@Thomass-MacBook-Air-2.local>
2015-09-07 17:38:27 -0400

    First Real commit

---
+++ codeforces/223C.cpp
@@ -0,0 +1,195 @@
+
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <time.h>
+#include <sys/time.h> 
+
+
+using namespace std;
+
+
+struct SegmentTreeNode {
+	int l,r,sum;
+	
+	void assignLeaf(char b) {
+		sum=0;
+		if(b == '(') {l=1;r=0;}
+		else {l=0;r=1;}
+	}
+	
+	void merge(SegmentTreeNode& left, SegmentTreeNode& right) {
+		l = left.l+right.l-min(left.l,right.r);
+		r =  right.r+left.r-min(left.l,right.r);
+		sum = left.sum+right.sum+min(left.l,right.r)*2;
+	}
+	
+	int getValue() {
+		return sum;
+	}
+};
+
+
+// T is the type of input array elements
+// V is the type of required aggregate statistic
+template<class T, class V>
+class SegmentTree {
+  SegmentTreeNode* nodes;
+  int N;
+
+public:
+  SegmentTree(T arr[], int N) {
+    this->N = N;
+    nodes = new SegmentTreeNode[getSegmentTreeSize(N)];
+    buildTree(arr, 1, 0, N-1);
+  }
+  
+  ~SegmentTree() {
+    delete[] nodes;
+  }
+  
+  V getValue(int lo, int hi) {
+    SegmentTreeNode result = getValue(1, 0, N-1, lo, hi);
+    return result.getValue();
+  }
+  
+  void update(int index, T value) {
+    update(1, 0, N-1, index, value);
+  }
+  
+  private:	
+  void buildTree(T arr[], int stIndex, int lo, int hi) {
+    if (lo == hi) {
+      nodes[stIndex].assignLeaf(arr[lo]);
+      return;
+    }
+    
+    int left = 2 * stIndex, right = left + 1, mid = (lo + hi) / 2;
+    buildTree(arr, left, lo, mid);
+    buildTree(arr, right, mid + 1, hi);
+    nodes[stIndex].merge(nodes[left], nodes[right]);
+  }
+  
+  SegmentTreeNode getValue(int stIndex, int left, int right, int lo, int hi) {
+    if (left == lo && right == hi)
+      return nodes[stIndex];
+    	
+    int mid = (left + right) / 2;
+    if (lo > mid)
+      return getValue(2*stIndex+1, mid+1, right, lo, hi);
+    if (hi <= mid)
+      return getValue(2*stIndex, left, mid, lo, hi);
+    	
+    SegmentTreeNode leftResult = getValue(2*stIndex, left, mid, lo, mid);
+    SegmentTreeNode rightResult = getValue(2*stIndex+1, mid+1, right, mid+1, hi);
+    SegmentTreeNode result;
+    result.merge(leftResult, rightResult);
+    return result;
+  }
+  
+  int getSegmentTreeSize(int N) {
+    int size = 1;
+    for (; size < N; size <<= 1);
+    return size << 1;
+  }
+  
+  void update(int stIndex, int lo, int hi, int index, T value) {
+    if (lo == hi) {
+    nodes[stIndex].assignLeaf(value);
+    return;
+    }
+    
+    int left = 2 * stIndex, right = left + 1, mid = (lo + hi) / 2;
+    if (index <= mid)
+      update(left, lo, mid, index, value);
+    else
+      update(right, mid+1, hi, index, value);
+    
+    nodes[stIndex].merge(nodes[left], nodes[right]);
+  }
+};
+
+/*
+    typedef unsigned long long timestamp_t;
+
+    static timestamp_t
+    get_timestamp ()
+    {
+      struct timeval now;
+      gettimeofday (&now, NULL);
+      return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
+    }
+*/
+
+int main ()
+{	
+	//timestamp_t t0 = get_timestamp();
+	int n,m,x,y; 
+	char A[1000005];
+	string s(100000,'(');
+	
+	getline(cin,s);
+	n=s.length();
+	strcpy(A, s.c_str());
+	
+	scanf("%d",&m);
+
+	SegmentTree<char,int> st(A,n);
+	
+	for (int i = 0; i < m; i += 1)
+	{
+		scanf("%d %d",&x,&y);
+		printf("%d\n",st.getValue(x-1,y-1));
+		
+	}
+	/*
+	timestamp_t t1 = get_timestamp();
+   	double secs = (t1 - t0) / 1000000.0L;
+   	cout << "secs: "<<secs << endl;
+	*/
+	return 0;
+}
